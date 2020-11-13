92ea18f5715f396a5abc66526481012ba3b1c12c
Ahsan Jamil <57091356+ahsanjamil306@users.noreply.github.com>
2020-11-13 23:47:07 +0500

    Update Child.js

--- src/Child.js
+++ src/Child.js
@@ -11,7 +11,7 @@ const Child = () => {
     return (
         <div>
             <h1>This is first child using Context api</h1>
-            <h4>Counter value is : {counterValue[0]}</h4>
+            <h2>Counter value is : {counterValue[0]}</h2>
             <button onClick={ () => {counterValue[1](++counterValue[0])}}>Increment</button>
             
         </div>
